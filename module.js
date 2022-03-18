5f57a9fd9a0e7818bfe24ee8f4e50e128fb338a8
karencumlat <karencumlat@gmail.com>
2022-03-18 10:57:33 -0700

    create index to access all projects

---
+++ module.js
@@ -0,0 +1,152 @@
+export const projects = [
+  {
+    name: 'Expanding Cards',
+  },
+  {
+    name: 'Progress Steps',
+  },
+  {
+    name: 'Rotating Navigation',
+  },
+  {
+    name: 'Hidden Search',
+  },
+  {
+    name: 'Blurry Loading',
+  },
+  {
+    name: 'Scroll Animation',
+  },
+  {
+    name: 'Split Landing Page',
+  },
+  {
+    name: 'Form Wave Animation',
+  },
+  {
+    name: 'Soundboard',
+  },
+  {
+    name: 'Dad Jokes',
+  },
+  {
+    name: 'Event Key Codes',
+  },
+  {
+    name: 'FAQ',
+  },
+  {
+    name: 'Random Choice Picker',
+  },
+  {
+    name: 'Animated Navigation',
+  },
+  {
+    name: 'Increment Counter',
+  },
+  {
+    name: 'Drink Water',
+  },
+  {
+    name: 'Movie App',
+  },
+  {
+    name: 'Background Slider',
+  },
+  {
+    name: 'Theme Clock',
+  },
+  {
+    name: 'Button Ripple Effect',
+  },
+  {
+    name: 'Drag and Drop',
+  },
+  {
+    name: 'Drawing App',
+  },
+  {
+    name: 'Kinetic Loader',
+  },
+  {
+    name: 'Content Placeholder',
+  },
+  {
+    name: 'Sticky Nav Bar',
+  },
+  {
+    name: 'Double Vertical Slider',
+  },
+  {
+    name: 'Toast Notification',
+  },
+  {
+    name: 'Github Profiles',
+  },
+  {
+    name: 'Double Heart Click',
+  },
+  {
+    name: 'Auto Text Effect',
+  },
+  {
+    name: 'Password Generator',
+  },
+  {
+    name: 'Good Cheap Fast',
+  },
+  {
+    name: 'Notes App',
+  },
+  {
+    name: 'Animated Countdown',
+  },
+  {
+    name: 'Image Carousel',
+  },
+  {
+    name: 'Hover Board',
+  },
+  {
+    name: 'Pokedex',
+  },
+  {
+    name: 'Mobile Tab Nav',
+  },
+  {
+    name: 'Password Strength Background',
+  },
+  {
+    name: '3D Boxes Background',
+  },
+  {
+    name: 'Verify Account',
+  },
+  {
+    name: 'Live Use Filter',
+  },
+  {
+    name: 'Feedback UI',
+  },
+  {
+    name: 'Custom Range Slider',
+  },
+  {
+    name: 'Netflix Navigation',
+  },
+  {
+    name: 'Quiz App',
+  },
+  {
+    name: 'Testimonial Box',
+  },
+  {
+    name: 'Random Image Feed',
+  },
+  {
+    name: 'ToDo List',
+  },
+  {
+    name: 'Catch The Insect',
+  },
+];
---
+++ script.js
@@ -0,0 +1,29 @@
+import { projects } from './module.js';
+const main = document.getElementById('main');
+showProjects(projects);
+function showProjects(projects) {
+  main.innerHTML = '';
+  projects.forEach((project, index) => {
+    const { name } = project;
+    const trim = name.toLowerCase().replace(/\s+/g, '');
+    const day = index + 1;
+    const link = index < 9 ? `project0${day}.${trim}` : `project${day}.${trim}`;
+
+    const projectEl = document.createElement('div');
+    projectEl.classList.add('project');
+
+    projectEl.innerHTML = `
+
+    <div class="card m-1" style="width: 18rem;">
+  <img src="./assets/image/${day}.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Day ${day}</h5>
+    <a href='${link}/index.html' class="card-link" target="_blank"><h6 class="card-subtitle">${name} Demo</h6></a>
+  </div>
+</div>
+
+  `;
+
+    main.appendChild(projectEl);
+  });
+}
